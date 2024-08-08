d59ae42492f2c65a7bbe7137e53701c3f6d9835a
Robert Jank <robert.jank@yahoo.com>
2024-08-08 10:40:45 -0500

    TodoList component now renders a list of todos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Form from "./Form";
+import TodoList from "./TodoList";
+import axios from "axios";
 
 const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get(URL)
+      .then((res) => {
+        this.setState({ todos: res.data.data });
+      })
+      .catch((err) => console.error(err));
+  }
   render() {
-    return null;
+    return (
+      <div>
+        <h1>Todos:</h1>
+        <TodoList todos={this.state.todos} />
+        <Form />
+      </div>
+    );
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return null
+    return (
+      <ul>
+        {this.props.todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+              }}
+            >
+              {todo.name}
+            </span>
+          </li>
+        ))}
+      </ul>
+    );
   }
 }
